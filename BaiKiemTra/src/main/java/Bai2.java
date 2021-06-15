ce78a1d8a497ea701ed1a8785a2d8b0ea44323aa
Van Nguyen <v.vannt34@vinid.net>
2021-06-15 11:06:33 +0700

    update

--- BaiKiemTra/src/main/java/Bai2.java
+++ BaiKiemTra/src/main/java/Bai2.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Bai2 {
 
     /*
-     *Viết chương trình nhập mật khẩu, yêu cầu mật khẩu phải có từ 10 ký tự trở lên trong đó có ít nhất 1 ký tự in hoa, 1 ký tự số, 1 ký tự đặc biệt như . , - _,…
-     *Nếu mật khẩu ít hơn 10 ký tự thông báo mật khẩu yếu
-     *Nếu thiếu một trong các ký tự trên thì thông báo mật khẩu chưa đủ mạnh
+     * Viết chương trình nhập mật khẩu, yêu cầu mật khẩu phải có từ 10 ký tự trở lên trong đó có ít nhất 1 ký tự in hoa, 1 ký tự số, 1 ký tự đặc biệt như . , - _,…
+     * Nếu mật khẩu ít hơn 10 ký tự thông báo mật khẩu yếu
+     * Nếu thiếu một trong các ký tự trên thì thông báo mật khẩu chưa đủ mạnh
      */
     public void giaiBKT2() {
         Bai2 bai2 = new Bai2();
@@ -15,8 +15,6 @@ public class Bai2 {
 
         if (bai2.isValid(password)) {
             System.out.println("Mật khẩu hợp lệ");
-        } else {
-            System.out.println("Mật khẩu không hợp lệ");
         }
     }
 
@@ -74,10 +72,9 @@ public class Bai2 {
 
     public boolean checkCapsChar(String password) {
         int count = 0;
-        for (int i = 65; i <= 90; i++) {
-            char c = (char) i;
-            String str1 = Character.toString(c);
-            if (password.contains(str1)) {
+        for (int i = 0; i < password.length(); i++) {
+            char ch = password.charAt(i);
+            if (Character.isUpperCase(ch)) {
                 count = 1;
             }
         }
@@ -89,9 +86,9 @@ public class Bai2 {
 
     public boolean checkDigitsChar(String password) {
         int count = 0;
-        for (int i = 0; i <= 9; i++) {
-            String str1 = Integer.toString(i);
-            if (password.contains(str1)) {
+        for (int i = 0; i < password.length(); i++) {
+            char ch = password.charAt(i);
+            if (Character.isDigit(ch)) {
                 count = 1;
             }
         }
@@ -103,11 +100,9 @@ public class Bai2 {
 
     public boolean checkSmallLetter(String password) {
         int count = 0;
-        for (int i = 90; i <= 122; i++) {
-            char c = (char) i;
-            String str1 = Character.toString(c);
-
-            if (password.contains(str1)) {
+        for (int i = 0; i < password.length(); i++) {
+            char ch = password.charAt(i);
+            if (Character.isLowerCase(ch)) {
                 count = 1;
             }
         }
@@ -117,4 +112,26 @@ public class Bai2 {
         return true;
     }
 
+    public boolean isValidPassword(String password) {
+        if (password.length() <= 10) {
+            System.out.println("Mật khẩu yếu. Mật khẩu phải có từ 10 ký tự trở lên");
+            return false;
+        }
+
+        if (password.contains(" ")) {
+            System.out.println("Mật khẩu không được chứa kí tự space");
+            return false;
+        }
+
+        if (!isMatchRegex(password)) {
+            System.out.println("Mật khẩu phải có ít nhất 1 ký tự in hoa, 1 ký tự số, 1 ký tự đặc biệt");
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean isMatchRegex(String password) {
+        return password.matches("\"^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$\"\n");
+    }
 }
